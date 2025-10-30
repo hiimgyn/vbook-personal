c0f6e96a970d73aaaee32ee8f3b8b0026743d74b
hiimgyn <nguyenminhhung.work@gmail.com>
2025-10-31 04:42:06 +0700

    re-version

--- nettruyen/src/home.js
+++ nettruyen/src/home.js
@@ -5,15 +5,14 @@ function execute() {
     
     return Response.success([
         { title: "Mới cập nhật", input: workingHost + "/tim-truyen", script: "gen.js" },
-        { title: "Truyện mới", input: workingHost + "/tim-truyen?status=-1&sort=15", script: "gen.js" },
-        { title: "Top all", input: workingHost + "/tim-truyen?status=-1&sort=10", script: "gen.js" },
-        { title: "Top tháng", input: workingHost + "/tim-truyen?status=-1&sort=11", script: "gen.js" },
-        { title: "Top tuần", input: workingHost + "/tim-truyen?status=-1&sort=12", script: "gen.js" },
-        { title: "Top ngày", input: workingHost + "/tim-truyen?status=-1&sort=13", script: "gen.js" },
-        { title: "Theo dõi", input: workingHost + "/tim-truyen?status=-1&sort=20", script: "gen.js" },
-        { title: "Bình luận", input: workingHost + "/tim-truyen?status=-1&sort=25", script: "gen.js" },
-        { title: "Số Chapter", input: workingHost + "/tim-truyen?status=-1&sort=30", script: "gen.js" },
-        { title: "Top Follow", input: workingHost + "/tim-truyen?status=-1&sort=19", script: "gen.js" },
-
+        { title: "Truyện mới", input: workingHost + "/tim-truyen?&sort=15", script: "gen.js" },
+        { title: "Top all", input: workingHost + "/tim-truyen?&sort=10", script: "gen.js" },
+        { title: "Top tháng", input: workingHost + "/tim-truyen?&sort=11", script: "gen.js" },
+        { title: "Top tuần", input: workingHost + "/tim-truyen?&sort=12", script: "gen.js" },
+        { title: "Top ngày", input: workingHost + "/tim-truyen?&sort=13", script: "gen.js" },
+        { title: "Theo dõi", input: workingHost + "/tim-truyen?&sort=20", script: "gen.js" },
+        { title: "Bình luận", input: workingHost + "/tim-truyen?&sort=25", script: "gen.js" },
+        { title: "Số Chapter", input: workingHost + "/tim-truyen?&sort=30", script: "gen.js" },
+        { title: "Top Follow", input: workingHost + "/tim-truyen?&sort=19", script: "gen.js" },
     ]);
 }
